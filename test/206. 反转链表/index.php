2db4dd58cc004eb224bcecf504528a309e657acd
蔡敏芳 <caiminfang@dstcar.com>
2020-09-25 15:57:55 +0800

    169. 多数元素

--- test/206. 反转链表/index.php
+++ test/206. 反转链表/index.php
@@ -1,7 +1,35 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: huahua
- * Date: 2020/9/25
- * Time: 下午3:48
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
  */
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function reverseList($head) {
+        $res = null;
+        $current = $head;
+        while($current){
+            $next = $current->next;
+            $current->next = $res;
+            $res = $current;
+            $current = $next;
+        }
+        return $res;
+    }
+}

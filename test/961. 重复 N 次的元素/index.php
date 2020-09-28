02d65cc36fff3b5b98611754a306d19e31eb5ed7
蔡敏芳 <caiminfang@dstcar.com>
2020-09-28 11:23:33 +0800

    961. 重复 N 次的元素

---
+++ test/961. 重复 N 次的元素/index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+
+在大小为 2N 的数组 A 中有 N+1 个不同的元素，其中有一个元素重复了 N 次。
+
+返回重复了 N 次的那个元素。
+
+
+
+示例 1：
+
+输入：[1,2,3,3]
+输出：3
+示例 2：
+
+输入：[2,1,2,5,3,2]
+输出：2
+示例 3：
+
+输入：[5,1,5,2,5,3,5,4]
+输出：5
+ */
+class Solution {
+
+    /**
+     * @param Integer[] $A
+     * @return Integer
+     */
+    function repeatedNTimes($A) {
+        $hash = [];
+        for ($i = 0,$count = count($A),$num = $count/2;$i <$count;$i++){
+            $hash[$A[$i]] =($hash[$A[$i]] ?? 0) +1;
+            if ($hash[$A[$i]] == $num){
+                return $A[$i];
+            }
+        }
+    }
+}

945923f0c3911abe396451297958d412c38e24dc
蔡敏芳 <caiminfang@dstcar.com>
2020-10-12 17:50:39 +0800

    167. 两数之和 II - 输入有序数组

---
+++ test/167. 两数之和 II - 输入有序数组/index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huahua
+ * Date: 2020/10/12
+ * Time: 下午5:49
+ * 167. 两数之和 II - 输入有序数组
+给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+
+说明:
+
+返回的下标值（index1 和 index2）不是从零开始的。
+你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+示例:
+
+输入: numbers = [2, 7, 11, 15], target = 9
+输出: [1,2]
+解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ */
+class Solution {
+
+    /**
+     * @param Integer[] $numbers
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($numbers, $target) {
+        $length = count($numbers);
+        $left = 0;
+        $right = $length-1;
+        while($left <$right){
+            $sum = $numbers[$left] +$numbers[$right];
+            if ($sum == $target){
+                return [++$left,++$right];
+            }elseif($sum >$target){
+                $right--;
+            }else{
+                $left++;
+            }
+        }
+        return [];
+    }
+}
